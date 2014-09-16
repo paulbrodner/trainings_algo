5c5a97520f143405d6707678cbf66a39a85948ee
Work Paul <development1.paul@gmail.com>
2014-09-16 15:09:33 +0300

    added namespace for Prints

--- lib/prints/numere.rb
+++ lib/prints/numere.rb
@@ -1,13 +1,15 @@
-class Numere
-  def self.pretty_print(numar)
-    @numar = (numar.to_i) + 1
+module Prints
+  class Numere
+    def self.pretty_print(numar)
+      @numar = (numar.to_i) + 1
 
-    puts "Pentru #{numar} avem urmatorul format:"
+      puts "Pentru #{numar} avem urmatorul format:"
 
-    (1..@numar).each do |count|
-      out = ""
-      (1..@numar-count).each { out+="#{count}" }
-      puts out
+      (1..@numar).each do |count|
+        out = ""
+        (1..@numar-count).each { out+="#{count}" }
+        puts out
+      end
     end
   end
 end
--- print.rb
+++ print.rb
@@ -2,4 +2,4 @@ require File.join(File.dirname(__FILE__),'lib/prints/numere')
 
 puts "Afiseaza format pentru X numere:"
 numar = gets.chomp
-Numere.pretty_print(numar)
+Prints::Numere.pretty_print(numar)
