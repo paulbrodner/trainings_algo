453bca8642f4dfdd7bdb2364cb733d5752eb59d3
Work Paul <development1.paul@gmail.com>
2014-09-16 20:31:00 +0300

    urmarim sa avem urmatoarele 2 triunghiuri “combinate” pentru cazul in care n=3: 1 2 2 3 3 3 si 3 3 3 2 2 1

--- lib/prints/numere.rb
+++ lib/prints/numere.rb
@@ -1,17 +1,19 @@
 module Prints
   class Numere
     def self.pretty_print(numar)
-      @numar = (numar.to_i) + 1
+      @numar = numar.to_i
 
       puts "Pentru #{numar} avem urmatorul format:"
-
-      (1..@numar).each do |count|
-        out = ""
-        (1..@numar-count).each { out+="#{count}" }
-        puts out
+      out = ""
+      @numar.downto(1) do |count|
+        tmp = ""
+        count.downto(1) { tmp+="#{count}" }
+        out += "#{tmp}\n"
       end
+      puts "#{out.reverse}\n#{out}"
     end
   end
+
 end
 
 
